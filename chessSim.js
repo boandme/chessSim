fe0e5da6fa00f0fe5ac7ffd1b768913ddd65e9cb
boandme <vyomj.home@gmail.com>
2024-08-30 23:17:20 -0700

    v0.05(Learn Update FINAL)

--- chessSim.js
+++ chessSim.js
@@ -3,7 +3,7 @@ var money =  100;
 var total_xp = 0;
 var current_xp = 0;
 var levelup_xp = 30;
-var level = 0;
+var level = 1;
 var current_width = 70;
 function OpenTab(et, TabName) {
   var i = 0
@@ -29,16 +29,70 @@ function puzzles() {
       money -= 3;
       total_xp += 3;
       current_xp += 3;
-      current_width += inc;
       var inc = Math.floor(1000/levelup_xp);
-      
-      document.getElementById("progress-bar-fill").style.width = String(current_width + "px");
+      current_width += inc * current_xp;
+      strwidth = String(current_width)
+      document.getElementById("progress-bar-fill").style.width = strwidth.concat("px")
       console.log(document.getElementById("progress-bar-fill"))
       document.getElementById("money").innerText = money;
-      // if (current_xp >= levelup_xp) {
-      //   level ++;
-      //   levelup_xp += 10;
-      // }
+      if (current_width >= 1000) {
+          level ++;
+          levelup_xp += 10;
+          current_width = 0;
+          strwidth = String(current_width);
+          document.getElementById("progress-bar-fill").style.width = strwidth.concat("px");
+          document.getElementById("below_level").innerText = level;
+          document.getElementById("upper_level").innerText = level + 1;
+       };
 
     }
+}
+
+function online_game() {
+  if ( money >= 5) {   
+    money -= 5;
+    total_xp += 6;
+    current_xp += 6;
+    var inc = Math.floor(1000/levelup_xp);
+    current_width += inc * current_xp;
+    strwidth = String(current_width)
+    document.getElementById("progress-bar-fill").style.width = strwidth.concat("px")
+    console.log(document.getElementById("progress-bar-fill"))
+    document.getElementById("money").innerText = money;
+    if (current_width >= 1000) {
+        level ++;
+        levelup_xp += 10;
+        current_width = 0;
+        strwidth = String(current_width);
+        document.getElementById("progress-bar-fill").style.width = strwidth.concat("px");
+        document.getElementById("below_level").innerText = level;
+        document.getElementById("upper_level").innerText = level + 1;
+     };
+
+  }
+}
+
+function chess_class() {
+  if ( money >= 15) {   
+    money -= 15;
+    total_xp += 18;
+    current_xp += 18;
+    var inc = Math.floor(1000/levelup_xp);
+    current_width += inc * current_xp;
+    strwidth = String(current_width)
+    document.getElementById("progress-bar-fill").style.width = strwidth.concat("px")
+    console.log(document.getElementById("progress-bar-fill"))
+    document.getElementById("money").innerText = money;
+    if (current_width >= 1000) {
+        level ++;
+        levelup_xp += 10;
+        current_width = 0;
+        current_xp = 0;
+        strwidth = String(current_width);
+        document.getElementById("progress-bar-fill").style.width = strwidth.concat("px");
+        document.getElementById("below_level").innerText = level;
+        document.getElementById("upper_level").innerText = level + 1;
+     };
+
+  }
 }
